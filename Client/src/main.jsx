40c97d9f71994367bca48ba5af4582f99f6d5706
PPEACH21 <sohizz1201@gmail.com>
2025-09-23 02:15:55 +0700

    refactor: remove unused component imports for cleaner code

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,13 +3,6 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
 
-import MapTest from './component/MapTest.jsx'
-import MapPickerButton from './component/MapPickerButton.jsx'
-import MenuStore from './page/MenuStore.jsx'
-import AddMenu from './component/AddMenu.jsx'
-import ShowMenu from './component/showMenu.jsx'
-import ShowImageExample from './component/ShowImageExample.jsx'
-
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
       <App />
