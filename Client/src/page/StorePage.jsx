bbc667107d33aa9c4ba550af0fe5ba28c29c1288
Antinomy5ds <dktv231@gmail.com>
2025-10-17 22:22:12 +0700

    edit

---
+++ Client/src/page/StorePage.jsx
@@ -0,0 +1,19 @@
+const StorePage = () => {
+  return (
+    <>
+      <nav style={{backgroundColor:"orange"}} >
+        <div className="nav">
+          <img src="https://i.ibb.co/MyMPRx3P/Chat-GPT-Image-Sep-23-2025-10-01-38-PM.jpg" ></img>
+        </div>
+      </nav>
+
+      {/* เนื้อหาอื่น */}
+      <div className="container mt-4">
+        <h1>Welcome to the Store</h1>
+        <p>เนื้อหาต่าง ๆ ของร้าน</p>
+      </div>
+    </>
+  );
+};
+
+export default StorePage;
